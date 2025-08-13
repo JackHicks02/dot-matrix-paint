98d0b77609f3105de70378f48ff366bd21aaba9d
Jack Hicks <jack.hicks@onebigcircle.co.uk>
2025-08-13 23:24:10 +0100

    feat(App): encode image on draw

--- src/App.tsx
+++ src/App.tsx
@@ -22,12 +22,19 @@ type UserState = {
   drawMode: DrawMode;
 };
 
+const getBit = (bit: number): [number, number] => {
+  const byte = Math.floor(bit / 8);
+  const offset = bit % 8;
+  return [byte, offset];
+};
+
 interface CircleProps {
   x: number;
   y: number;
   pixelsRef: RefObject<Record<string, SVGCircleElement>>;
-  bitMapRef: RefObject<number[]>;
+  bitMapRef: RefObject<Uint8Array>;
   userStateRef: RefObject<UserState>;
+  handleDraw: () => void;
 }
 
 const Circle: FC<CircleProps> = ({
@@ -36,24 +43,38 @@ const Circle: FC<CircleProps> = ({
   pixelsRef,
   bitMapRef,
   userStateRef,
+  handleDraw,
 }) => {
-  const [colour, setColour] = useState("grey");
-
+  const [colour, setColour] = useState("darkgrey");
   const index = y * DOTMATRIX_SIDE + x;
 
-  const handleMouseOver = () => {
+  const paint = () => {
     const userState = userStateRef.current;
+    const [byte, offset] = getBit(index);
+    const mask = 1 << offset;
+    if (userState.drawMode === DrawMode.DRAW) {
+      bitMapRef.current[byte] |= mask;
+      setColour("red");
+    } else {
+      bitMapRef.current[byte] &= ~mask;
+      setColour("darkgrey");
+    }
 
-    if (!userState.mouseDown) return;
-    const newColour = userState.drawMode === 0 ? "red" : "grey";
+    handleDraw();
+  };
 
-    setColour(newColour);
-    bitMapRef.current[index] = 1;
+  const handleMouseOver = () => {
+    if (!userStateRef.current.mouseDown) return;
+    paint();
+  };
+
+  const handleMouseDown = () => {
+    userStateRef.current.mouseDown = true;
+    paint();
   };
 
   return (
     <circle
-      key={`${x}-${y}`}
       cx={x * (SCALE + SPACING) + SCALE / 2}
       cy={y * (SCALE + SPACING) + SCALE / 2}
       r={SCALE / 2}
@@ -62,13 +83,25 @@ const Circle: FC<CircleProps> = ({
         if (el) pixelsRef.current[`${x}-${y}`] = el;
       }}
       onMouseOver={handleMouseOver}
+      onMouseDown={handleMouseDown}
     />
   );
 };
 
 function App() {
   const pixelsRef = useRef<Record<string, SVGCircleElement>>({});
-  const bitMapRef = useRef(Array(DOTMATRIX_SIDE * DOTMATRIX_SIDE).fill(0));
+  const bitMapRef = useRef(new Uint8Array(512));
+  const textFieldRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const encode = () => {
+    if (!textFieldRef.current) return;
+    const bytes = bitMapRef.current;
+    let binary = "";
+    for (let i = 0; i < bytes.length; i++) binary += bytes[i];
+    // const base64 = btoa(binary);
+
+    textFieldRef.current.value = binary;
+  };
 
   const userStateRef = useRef<UserState>({
     mouseDown: false,
@@ -79,14 +112,11 @@ function App() {
     const handleMouseDown = () => {
       userStateRef.current.mouseDown = true;
     };
-
     const handleMouseUp = () => {
       userStateRef.current.mouseDown = false;
     };
-
     window.addEventListener("mousedown", handleMouseDown);
     window.addEventListener("mouseup", handleMouseUp);
-
     return () => {
       window.removeEventListener("mousedown", handleMouseDown);
       window.removeEventListener("mouseup", handleMouseUp);
@@ -98,11 +128,13 @@ function App() {
     for (let x = 0; x < DOTMATRIX_SIDE; x++) {
       pixels.push(
         <Circle
+          key={`${x}-${y}`}
           x={x}
           y={y}
           pixelsRef={pixelsRef}
           bitMapRef={bitMapRef}
           userStateRef={userStateRef}
+          handleDraw={encode}
         />
       );
     }
@@ -118,8 +150,21 @@ function App() {
       >
         {pixels}
       </svg>
-      <div className="h-full">
-        <h1>Base 64 output</h1>
+      <div className="h-full flex flex-col w-[600px]">
+        <div className="flex gap-2 items-center">
+          <h1 className="text-2xl">Base 64 output</h1>
+          <button
+            className="bg-slate-400 border border-amber-400 px-2 py-1 rounded-md hover:brightness-125 cursor-pointer"
+            onClick={encode}
+          >
+            Encode
+          </button>
+        </div>
+        <textarea
+          className="w-full h-full border-slate-500 border p-1"
+          ref={textFieldRef}
+          defaultValue=""
+        />
       </div>
       <div className="absolute right-2 top-2 flex flex-col gap-4 bg-slate-600 p-2 rounded-md">
         <button
